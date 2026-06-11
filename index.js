7400d048db770065a12649d0f731aa4f6b06348f
İrfan Batuhan Aksu <72353097+Nettrox@users.noreply.github.com>
2026-06-11 17:01:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 import { createNewSession } from "./func/createNewSession.js";
-// import { getInput } from "./func/getInput.js";
+import { getInput } from "./func/getInput.js";
 import { saveCurrentSession } from "./func/saveCurrentSession.js";
 import { newAskOdysseus } from "./func/newAskOdysseus.js";
 import { getLatestSessionId } from "./func/getLatestSessionId.js";
 import {oldAskOdysseus} from "./func/oldAskOdysseus.js";
 
-// const question = await getInput();
+//get input from user (this func will not be used for implementation projects)
+const question = await getInput();
 
+// new chat (func for create a new chat)
 const sessionId = await createNewSession();
 const lastSessionId = await getLatestSessionId();
-
-console.log("Old Session ID:", lastSessionId);
+await newAskOdysseus(sessionId, question);
+await saveCurrentSession(sessionId);
 console.log("New Session ID:", sessionId);
+
+// latest chat (if there is a existing chat this func can be used)
+await oldAskOdysseus(lastSessionId, question);
+console.log("Old Session ID:", lastSessionId);
+
+// print response and user input
 console.log("Question:", question);
 console.log("Answer:");
 
-// new chat
-await newAskOdysseus(sessionId, question);
-await saveCurrentSession(sessionId);
 
-// latest chat
-await oldAskOdysseus(lastSessionId, question);
+
